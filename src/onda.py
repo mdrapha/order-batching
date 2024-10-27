f1852527f7ae34aa736050918531c482b2cd662b
Davi Seiji Kawai Santos <daviseiji@gmail.com>
2024-10-27 14:15:23 -0300

    Add pytest

--- src/onda.py
+++ src/onda.py
@@ -8,11 +8,9 @@ class Onda:
 
     def add_caixa(self, caixa: Caixa):
         self.caixas.append(caixa)
-        print(f'Caixa {caixa.get_id()} adicionada na onda {self.id}')
 
     def remove_caixa(self, caixa: Caixa):
         self.caixas.remove(caixa)
-        print(f'Caixa {caixa.get_id()} removida da onda {self.id}')
 
     def get_caixas(self):
         return self.caixas
